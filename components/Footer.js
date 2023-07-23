5529835f76e717d88caa1b37e42e0492b5a67073
Bismark Arroyo <bis.arroyo@gmail.com>
2023-07-23 13:09:26 -0600

    Add footer items

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,54 @@
-export default
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import styles from './styles/footer.module.css'
+
+// home icon
+import { RiHome2Line } from 'react-icons/ri'
+
+// home icon fill
+import { RiHome2Fill } from 'react-icons/ri'
+
+// message icons
+import { RiChat4Line } from 'react-icons/ri'
+
+// message icon fill
+import { RiChat4Fill } from 'react-icons/ri'
+
+// profile icon
+import { BsPerson } from 'react-icons/bs'
+
+// profile icon fill
+import { BsPersonFill } from 'react-icons/bs'
+
+export default function Footer() {
+  const path = usePathname()
+  console.log(path)
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.home}>
+        {path === '/' ? (
+          <RiHome2Fill className={styles.homeIcon} />
+        ) : (
+          <RiHome2Line className={styles.homeIcon} />
+        )}
+      </div>
+      <div className={styles.messages}>
+        {path === '/messages' ? (
+          <RiChat4Fill className={styles.messagesIcon} />
+        ) : (
+          <RiChat4Line className={styles.messagesIcon} />
+        )}
+      </div>
+      <div className={styles.profile}>
+        {path === '/profile' ? (
+          <BsPersonFill className={styles.profileIcon} />
+        ) : (
+          <BsPerson className={styles.profileIcon} />
+        )}
+      </div>
+    </footer>
+  )
+}
