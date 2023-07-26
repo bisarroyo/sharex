6fc3a372c975a346279d4c94efb78a4a8b98a368
Bismark Arroyo <bis.arroyo@gmail.com>
2023-07-25 22:22:51 -0600

    add links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -27,40 +27,49 @@ import { BsPlusCircle } from 'react-icons/bs'
 
 // create icon fill
 import { BsPlusCircleFill } from 'react-icons/bs'
+import Link from 'next/link'
 
 export default function Footer() {
   const path = usePathname()
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.home}>
-        {path === '/' ? (
-          <RiHome2Fill className={styles.homeIcon} />
-        ) : (
-          <RiHome2Line className={styles.homeIcon} />
-        )}
-      </div>
-      <div className={styles.messages}>
-        {path === '/upload' ? (
-          <BsPlusCircle className={styles.createIcon} />
-        ) : (
-          <BsPlusCircleFill className={styles.createIcon} />
-        )}
-      </div>
-      <div className={styles.messages}>
-        {path === '/messages' ? (
-          <RiChat4Fill className={styles.messagesIcon} />
-        ) : (
-          <RiChat4Line className={styles.messagesIcon} />
-        )}
-      </div>
-      <div className={styles.profile}>
-        {path === '/profile' ? (
-          <BsPersonFill className={styles.profileIcon} />
-        ) : (
-          <BsPerson className={styles.profileIcon} />
-        )}
-      </div>
+      <Link href='/'>
+        <div className={styles.home}>
+          {path === '/' ? (
+            <RiHome2Fill className={styles.homeIcon} />
+          ) : (
+            <RiHome2Line className={styles.homeIcon} />
+          )}
+        </div>
+      </Link>
+      <Link href='/upload'>
+        <div className={styles.messages}>
+          {path === '/upload' ? (
+            <BsPlusCircleFill className={styles.createIcon} />
+          ) : (
+            <BsPlusCircle className={styles.createIcon} />
+          )}
+        </div>
+      </Link>
+      <Link href='/messages'>
+        <div className={styles.messages}>
+          {path === '/messages' ? (
+            <RiChat4Fill className={styles.messagesIcon} />
+          ) : (
+            <RiChat4Line className={styles.messagesIcon} />
+          )}
+        </div>
+      </Link>
+      <Link href='/profile'>
+        <div className={styles.profile}>
+          {path === '/profile' ? (
+            <BsPersonFill className={styles.profileIcon} />
+          ) : (
+            <BsPerson className={styles.profileIcon} />
+          )}
+        </div>
+      </Link>
     </footer>
   )
 }
