244744b2b1528fb2fc126be562fcd5b13bd95b56
Bismark Arroyo <bis.arroyo@gmail.com>
2023-08-08 21:22:09 -0600

    Add component to post images

--- components/PostImage.js
+++ components/PostImage.js
@@ -1,25 +1,7 @@
 'use client'
 
-import { useRef, useState } from 'react'
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-// import required modules
-import { Pagination, Navigation } from 'swiper/modules'
-
-import Image from 'next/image'
-import { getCldImageUrl } from 'next-cloudinary'
-
-// Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-
-// Customize swiper
-import './styles/swiper.css'
-
 import styles from './styles/postimage.module.css'
+import SwiperComponent from './SwiperComponent'
 
 export default function PostImage({ postImages }) {
   let sliderImages = []
@@ -33,34 +15,5 @@ export default function PostImage({ postImages }) {
   //   sliderImages.push(url)
   // })
 
-  return (
-    <Swiper
-      pagination={{
-        type: 'fraction'
-      }}
-      navigation={false}
-      modules={[Pagination, Navigation]}
-      className='mySwiper'
-    >
-      {sliderImages.map((image, index) => {
-        return (
-          <SwiperSlide key={index} className={styles.slide}>
-            <Image src={image} width='500' height='600' alt='image post' />
-          </SwiperSlide>
-        )
-      })}
-      {/* {postImages.map((image, index) => {
-        return (
-          <SwiperSlide key={image.id} className={styles.slide}>
-            <CldImage
-              src={image.urlCloud.publicID}
-              width='500'
-              height='600'
-              alt='image post'
-            />
-          </SwiperSlide>
-        )
-      })} */}
-    </Swiper>
-  )
+  return <SwiperComponent images={sliderImages} />
 }
