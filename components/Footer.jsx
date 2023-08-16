c1806b42c1f82be6fb226b9c8457d5b117144696
Bismark Arroyo <bis.arroyo@gmail.com>
2023-08-15 21:33:30 -0600

    Added icons to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,71 +1,51 @@
 'use client'
 
+import {
+  IconHome,
+  IconHomeCheck,
+  IconSquareRoundedPlus,
+  IconSquareRoundedPlusFilled,
+  IconMessageCircle2,
+  IconMessageCircle2Filled,
+  IconUser,
+  IconUserCheck
+} from '@tabler/icons-react'
+
 import { usePathname } from 'next/navigation'
 
-// home icon
-import { RiHome2Line } from 'react-icons/ri'
-
-// home icon fill
-import { RiHome2Fill } from 'react-icons/ri'
-
-// message icons
-import { RiChat4Line } from 'react-icons/ri'
-
-// message icon fill
-import { RiChat4Fill } from 'react-icons/ri'
-
-// profile icon
-import { BsPerson } from 'react-icons/bs'
-
-// profile icon fill
-import { BsPersonFill } from 'react-icons/bs'
-
-// create icon
-import { BsPlusCircle } from 'react-icons/bs'
-
-// create icon fill
-import { BsPlusCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
 
 export default function Footer() {
   const path = usePathname()
 
   return (
-    <footer className='fixed bottom-0 h-[60px] w-screen backdrop-blur-lg flex flex-row justify-around items-center z-10'>
+    <footer className='fixed bottom-0 h-[60px] w-screen backdrop-blur-lg bg-gray-950/[.9] flex flex-row justify-around items-center z-10'>
       <Link href='/'>
         <div className=''>
-          {path === '/' ? (
-            <RiHome2Fill className='' />
-          ) : (
-            <RiHome2Line className='' />
-          )}
+          {path === '/' ? <IconHomeCheck /> : <IconHome />}
         </div>
       </Link>
       <Link href='/upload'>
         <div className=''>
           {path === '/upload' ? (
-            <BsPlusCircleFill className='' />
+            <IconSquareRoundedPlusFilled />
           ) : (
-            <BsPlusCircle className='' />
+            <IconSquareRoundedPlus />
           )}
         </div>
       </Link>
       <Link href='/messages'>
         <div className=''>
           {path === '/messages' ? (
-            <RiChat4Fill className='' />
+            <IconMessageCircle2Filled />
           ) : (
-            <RiChat4Line className='' />
+            <IconMessageCircle2 />
           )}
         </div>
       </Link>
       <Link href='/profile'>
         <div className=''>
-          {path === '/profile' ? (
-            <BsPersonFill className='' />
-          ) : (
-            <BsPerson className='' />
-          )}
+          {path === '/profile' ? <IconUserCheck /> : <IconUser />}
         </div>
       </Link>
     </footer>
