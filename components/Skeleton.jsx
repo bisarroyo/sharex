0ca69a00a144f9dad8c7a4ce695d37c07fe325c6
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-10 20:54:20 -0600

    Fix loading screen

--- components/Skeleton.jsx
+++ components/Skeleton.jsx
@@ -3,9 +3,27 @@ import { Card } from '@nextui-org/card'
 
 export default function SkeletonPlaceholder() {
   return (
-    <Card className='w-[400px] space-y-5 p-4' radius='2xl'>
+    <Card
+      className='w-full h-screen space-y-5 p-4'
+      radius='none'
+      isBlurred={true}
+    >
       <Skeleton className='rounded-lg'>
-        <div className='h-24 rounded-lg bg-default-300'></div>
+        <div className='h-60 rounded-lg bg-default-300'></div>
+      </Skeleton>
+      <div className='space-y-3'>
+        <Skeleton className='w-3/5 rounded-lg'>
+          <div className='h-3 w-3/5 rounded-lg bg-default-200'></div>
+        </Skeleton>
+        <Skeleton className='w-4/5 rounded-lg'>
+          <div className='h-3 w-4/5 rounded-lg bg-default-200'></div>
+        </Skeleton>
+        <Skeleton className='w-2/5 rounded-lg'>
+          <div className='h-3 w-2/5 rounded-lg bg-default-300'></div>
+        </Skeleton>
+      </div>
+      <Skeleton className='rounded-lg'>
+        <div className='h-60 rounded-lg bg-default-300'></div>
       </Skeleton>
       <div className='space-y-3'>
         <Skeleton className='w-3/5 rounded-lg'>
